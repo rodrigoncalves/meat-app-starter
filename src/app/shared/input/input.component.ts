a95aa4221a6a673b010d5eae4b74b3cb1ca4c0fd
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-06-20 22:41:38 -0300

    LoginComponent showTip

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -11,6 +11,7 @@ export class InputComponent implements OnInit, AfterContentInit {
 
   @Input() label: string;
   @Input() errorMessage: string;
+  @Input() showTip: boolean = true;
 
   @ContentChild(NgModel) model: NgModel;
   @ContentChild(FormControlName) control: FormControlName;
