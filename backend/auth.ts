e49cf42c71d67b19f164d7772a9cafeb017d9dbd
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-06-20 21:35:26 -0300

    JWT

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express'
 import { User, users } from './users'
 
+import * as jwt from 'jsonwebtoken'
+
 export const handleAuthentication = (req: Request, res: Response) => {
   const user: User = req.body
   if (isValid(user)) {
     const dbUser = users[user.email]
+    const token = jwt.sign({
+      sub: dbUser.email,
+      iss: 'meat-api'
+    }, 'meat-api-password')
+
     res.json({
       name: dbUser.name,
-      email: dbUser.email
+      email: dbUser.email,
+      accessToken: token
     })
   } else {
     res.status(403).json({ message: 'Dados inválidos.' })
