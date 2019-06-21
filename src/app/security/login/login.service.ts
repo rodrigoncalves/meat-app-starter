e0ee52fd98d9c0f9d863303018a875b471814bca
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-06-20 22:33:15 -0300

    save loggedUser

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -4,12 +4,21 @@ import { Observable } from "rxjs/Observable";
 import { MEAT_API } from "app/app.api";
 import { User } from "./user.model";
 
+import 'rxjs/add/operator/do';
+
 @Injectable()
 export class LoginService {
+
+  loggedUser: User;
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${MEAT_API}/login`, { email, password });
+    return this.http.post<User>(`${MEAT_API}/login`, { email, password })
+      .do(user => this.loggedUser = user);
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedUser !== undefined;
   }
 }
