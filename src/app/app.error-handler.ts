86a85406246a6495448c193b766ce17236ab32b1
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-06-24 21:37:05 -0300

    NgZone

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,32 +1,35 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
 import { NotificationService } from "./shared/messages/notification.service";
 import { LoginService } from "./security/login/login.service";
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-  constructor(private ns: NotificationService, private injector: Injector) {
+  constructor(private ns: NotificationService,
+    private injector: Injector,
+    private zone: NgZone) {
     super();
   }
 
   handleError(errorResponse: HttpErrorResponse | any) {
     if (errorResponse instanceof HttpErrorResponse) {
       const message = errorResponse.error.message;
-      switch (errorResponse.status) {
-        case 401:
-          this.ns.notify(message || 'Não autenticado');
-          this.injector.get(LoginService).handleLogin();
-          break;
-        case 403:
-          this.ns.notify(message || 'Não autorizado');
-          break;
-        case 404:
-          this.ns.notify(message || 'Não encontrado');
-          break;
-      }
+      this.zone.run(() => {
+        switch (errorResponse.status) {
+          case 401:
+            this.ns.notify(message || 'Não autenticado');
+            this.injector.get(LoginService).handleLogin();
+            break;
+          case 403:
+            this.ns.notify(message || 'Não autorizado');
+            break;
+          case 404:
+            this.ns.notify(message || 'Não encontrado');
+            break;
+        }
+      });
     }
-
     super.handleError(errorResponse);
   }
 }
